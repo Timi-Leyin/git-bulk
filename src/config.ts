ffe24068fd27273f27037e5d1b61b96361060cbd
Timi-Leyin <timileyinoyelekan11@gmail.com>
2023-04-11 00:37:43 +0100

    refactor: fill up queries

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,15 @@ export default {
     name:"Git Bulk",
     version:"1.0.0",
     description:"Clone multiple repositories from git",
+
+    commands:{
+        clone:{
+            command:"clone",
+            description:"Clone repositories",
+            argument:["<username>", "User profile to clone from"],
+            options:{
+                provider:["-P, --provider <char>","Provider where cloning from, Github is default value","github"]
+            }
+        }
+    }
 }
