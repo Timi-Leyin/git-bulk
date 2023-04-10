f4e781d550121210953cfa32b5ba7cd965e27642
Timi-Leyin <timileyinoyelekan11@gmail.com>
2023-04-10 22:10:42 +0100

    **

--- src/cli.ts
+++ src/cli.ts
@@ -3,8 +3,8 @@ import cliSpinners from "cli-spinners";
 import { Command } from "commander";
 import config from "./config";
 import Loader from "./utils/loader";
-import checkbox from "@inquirer/checkbox"
-import {spawn} from "child_process"
+import checkbox from "@inquirer/checkbox";
+import { spawn } from "child_process";
 const program = new Command();
 
 program
@@ -19,19 +19,19 @@ program
   .command("clone")
   .description("Clone repositories ")
   .argument("<username>", "User profile to clone from")
-  .action(async(str, option) => {
+  .action(async (str, option) => {
     const init = new Loader(cliSpinners.aesthetic);
-    init.load("Initializing")
+    init.load("Initializing");
     // console.log(checkbox)
-    const gitClone = spawn("git",["--version"])
-    
-    gitClone.stdout.on("data",(data)=>{
-      init.text("Using "+data,1000)
+    const gitClone = spawn("git", ["--version"]);
+
+    gitClone.stdout.on("data", (data) => {
+      init.text("Using " + data, 1000);
       // loading.load("Cloning")
-    })
-    gitClone.on("error",(error)=>{
-      init.text("Exiting").stop(2000)
-    })
+    });
+    gitClone.on("error", (error) => {
+      init.text("Exiting").stop(2000);
+    });
   });
 
 export default program;
